368d6e278979700c1e10655aec05ecc84d2ceaeb
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 18:01:13 +0530

    roleback class

--- GA4GH/src/main/java/com/ga4gh/prototype/entity/GA4GH.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/entity/GA4GH.java
@@ -1,5 +1,17 @@
 package com.ga4gh.prototype.entity;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter	
+@Setter	
+@Entity
+@Table(name = "ga4gh")
 public class GA4GH {
 
 }
