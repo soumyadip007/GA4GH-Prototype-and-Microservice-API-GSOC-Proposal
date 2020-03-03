49262cefa9cef74e9066165c68b376f89c4ce348
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 21:30:11 +0530

    JUNIT Dependency added

--- GA4GH/src/test/java/com/ga4gh/prototype/testing/GA4GH_Test.java
+++ GA4GH/src/test/java/com/ga4gh/prototype/testing/GA4GH_Test.java
@@ -1,5 +1,14 @@
 package com.ga4gh.prototype.testing;
 
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 public class GA4GH_Test {
 
 }
