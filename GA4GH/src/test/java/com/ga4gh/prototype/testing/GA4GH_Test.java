4def24e9e5fc6b90a635861b91a94d6586ff9f6d
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 21:35:36 +0530

    Testing setup

--- GA4GH/src/test/java/com/ga4gh/prototype/testing/GA4GH_Test.java
+++ GA4GH/src/test/java/com/ga4gh/prototype/testing/GA4GH_Test.java
@@ -7,6 +7,16 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+
 public class GA4GH_Test {
 
+    private static final String SEQNAME = "chrom1";
+    
+    private static final String GENBANK = "CM000994.2";
+    
+    private static final String REFSEQ = "NC_000067.6";
+    
+    private static final String SEQUENCE_LENGTH = "NC_000067.6";
+    
+    
 }
