cb15802b0fe64a14c01155f9fcfddc2093a80d45
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 21:33:14 +0530

    JUNIT Import

--- GA4GH/src/test/java/com/ga4gh/prototype/testing/GA4GH_Test.java
+++ GA4GH/src/test/java/com/ga4gh/prototype/testing/GA4GH_Test.java
@@ -1,6 +1,5 @@
 package com.ga4gh.prototype.testing;
 
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,7 +7,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-
 public class GA4GH_Test {
 
 }
