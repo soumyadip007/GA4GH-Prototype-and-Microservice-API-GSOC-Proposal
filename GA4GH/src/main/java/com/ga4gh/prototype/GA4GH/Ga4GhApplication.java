9b79ad5836c84b69d1d73e6daaeb1406bdb0eb4d
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 17:06:51 +0530

    GA4GH Project Setup MAVEN

---
+++ GA4GH/src/main/java/com/ga4gh/prototype/GA4GH/Ga4GhApplication.java
@@ -0,0 +1,13 @@
+package com.ga4gh.prototype.GA4GH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ga4GhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ga4GhApplication.class, args);
+	}
+
+}
---
+++ GA4GH/src/test/java/com/ga4gh/prototype/GA4GH/Ga4GhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga4gh.prototype.GA4GH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ga4GhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
