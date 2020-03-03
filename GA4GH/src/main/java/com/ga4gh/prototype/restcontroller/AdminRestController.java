cce78ca68c72375df94f7eebabdd1103ac721fe1
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 18:34:06 +0530

    FindAll() added

--- GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/AdminRestController.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/AdminRestController.java
@@ -1,11 +1,14 @@
 package com.ga4gh.prototype.restcontroller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ga4gh.prototype.dao.GA4GHRepository;
+import com.ga4gh.prototype.entity.GA4GH;
 import com.ga4gh.prototype.service.GA4GHService;
 
 @CrossOrigin(origins ="*",allowedHeaders="*",maxAge=200000) 
@@ -23,4 +26,11 @@ public class AdminRestController {
 		this.GA4GHService=GA4GHService;
 	}
 	
+	
+
+	@GetMapping("/")
+	public List<GA4GH> FindAll() {
+	
+		return GA4GHService.findAll();
+	}
 }
