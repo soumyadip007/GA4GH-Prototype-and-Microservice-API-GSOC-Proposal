f62f1a6a937de93bd5adfe1ae03f83633fc78275
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 18:12:51 +0530

    All unimplemented methods added (Generated methods)

--- GA4GH/src/main/java/com/ga4gh/prototype/service/GA4GHService.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/service/GA4GHService.java
@@ -1,8 +1,32 @@
 package com.ga4gh.prototype.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.ga4gh.prototype.entity.GA4GH;
+
 @Service
-public class GA4GHService implements {
+public class GA4GHService implements GA4GHServiceInterface {
+
+	public List<GA4GH> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void save(GA4GH OBJ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteById(int theId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public GA4GH findById(int theId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
