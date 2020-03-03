2b55b5b86313d80b790967c690041edb2764bebd
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 18:47:01 +0530

    pUBLIC ADDED

--- GA4GH/src/main/java/com/ga4gh/prototype/dao/GA4GHRepository.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/dao/GA4GHRepository.java
@@ -11,11 +11,11 @@ import com.ga4gh.prototype.entity.GA4GH;
 @Repository("GA4GHRepository")
 public interface GA4GHRepository extends JpaRepository<GA4GH, Integer> {
 
-	GA4GH findById(int id);
+	public GA4GH findById(int id);
 	
-	List<GA4GH> findBySequence_Name(String seq);
+	public List<GA4GH> findBySequence_Name(String seq);
 	
-	List<GA4GH> findByGenBank_Accn(String seq);
+	public List<GA4GH> findByGenBank_Accn(String seq);
 	
-	List<GA4GH> findByRefseq_Accn(String seq);
+	public List<GA4GH> findByRefseq_Accn(String seq);
 }
