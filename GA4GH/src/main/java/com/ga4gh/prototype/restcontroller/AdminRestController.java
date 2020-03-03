750459a47e6ee82861f34f7c3f60dd9ad1ac7d85
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 18:35:07 +0530

    findById(@PathVariable int theId) added

--- GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/AdminRestController.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/AdminRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +33,12 @@ public class AdminRestController {
 	public List<GA4GH> FindAll() {
 	
 		return GA4GHService.findAll();
+	}
+	
+	
+	@GetMapping("/id/{id}")
+	public GA4GH findById(@PathVariable int theId) {
+		
+		return GA4GHService.findById(theId);
 	}
 }
