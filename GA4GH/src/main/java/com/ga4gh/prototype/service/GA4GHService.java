9a14efd8e7c6c408af13c8bf03d48c078e6f80b7
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 18:15:30 +0530

    autowiring and dependency indejection added

--- GA4GH/src/main/java/com/ga4gh/prototype/service/GA4GHService.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/service/GA4GHService.java
@@ -2,13 +2,26 @@ package com.ga4gh.prototype.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ga4gh.prototype.dao.GA4GHRepository;
 import com.ga4gh.prototype.entity.GA4GH;
 
 @Service
 public class GA4GHService implements GA4GHServiceInterface {
 
+	
+	private GA4GHRepository GA4GHRepository;
+	
+
+	@Autowired   
+	public GA4GHService(GA4GHRepository GA4GHRepository)
+	{
+		this.GA4GHRepository=GA4GHRepository;
+	}
+	
+	
 	public List<GA4GH> findAll() {
 		return null;
 	}
