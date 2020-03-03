82d2cbc982d49203e0439a32fa4ad2829be6ff23
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 18:04:57 +0530

    ga4gh repo added

--- GA4GH/src/main/java/com/ga4gh/prototype/dao/GA4GHRepository.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/dao/GA4GHRepository.java
@@ -1,9 +1,13 @@
 package com.ga4gh.prototype.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ga4gh.prototype.entity.GA4GH;
 
 
-public interface GA4GHRepository extends JpaRepository<Ga4GH, Integer> {
+@Repository("GA4GHRepository")
+public interface GA4GHRepository extends JpaRepository<GA4GH, Integer> {
 
-	Ga4GH findById(int id);
+	GA4GH findById(int id);
 }
