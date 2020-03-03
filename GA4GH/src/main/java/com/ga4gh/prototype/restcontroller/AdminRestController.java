79ff3fcc47c16234394474b669757f15ea6121b6
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 18:42:33 +0530

    save and update added

--- GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/AdminRestController.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/AdminRestController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -58,5 +60,25 @@ public class AdminRestController {
 	public List<GA4GH> findByRefseq_Accn(@PathVariable String theId) {
 		
 		return GA4GHService.findByRefseq_Accn(theId);
+	}
+	
+	
+
+	@PostMapping("/add")
+	public GA4GH add(@RequestBody GA4GH obj) {
+		
+		obj.setId(0);
+		
+		GA4GHService.save(obj);
+		
+		return obj;
+	}
+	
+	@PostMapping("/update")
+	public GA4GH update(@RequestBody GA4GH obj) {
+		
+		GA4GHService.save(obj);
+		
+		return obj;
 	}
 }
