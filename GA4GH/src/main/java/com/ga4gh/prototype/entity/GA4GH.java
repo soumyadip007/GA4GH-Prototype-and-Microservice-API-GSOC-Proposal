e45f8490f051c3a6ef8512bbbf1b969dd8d477fe
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 18:03:54 +0530

    Final Entity Added

--- GA4GH/src/main/java/com/ga4gh/prototype/entity/GA4GH.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/entity/GA4GH.java
@@ -1,9 +1,12 @@
 package com.ga4gh.prototype.entity;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,4 +17,18 @@ import lombok.Setter;
 @Table(name = "ga4gh")
 public class GA4GH {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	int id;		
+
+
+	@Column(name = "Sequence_Name")
+	String Sequence_Name;	
+	
+	@Column(name = "GenBank_Accn")
+	String GenBank_Accn;	
+	
+	@Column(name = "Refseq_Accn")
+	String Refseq_Accn;		
 }
