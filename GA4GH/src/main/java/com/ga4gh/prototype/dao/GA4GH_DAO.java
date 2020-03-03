618d94a154a20f8e71bfb9551c02deb5eead26da
soumyadip007 <soumyadip.note@gmail.com>
2020-03-03 17:49:16 +0530

    Table name added

--- GA4GH/src/main/java/com/ga4gh/prototype/dao/GA4GH_DAO.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/dao/GA4GH_DAO.java
@@ -1,10 +1,15 @@
 package com.ga4gh.prototype.dao;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "ga4gh")
 public class GA4GH_DAO {
 
 }
