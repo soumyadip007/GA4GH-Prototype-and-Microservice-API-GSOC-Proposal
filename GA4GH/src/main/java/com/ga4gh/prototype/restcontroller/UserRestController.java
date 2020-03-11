b8c07a6437d58d809a857b9a4a5a3731ecfc5f57
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 00:27:35 +0530

    Static API Key for headers

--- GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/UserRestController.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/UserRestController.java
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,15 +44,36 @@ public class UserRestController {
 	}
 	
 	@GetMapping("/sequencename/{theId}")
-	public List<GA4GH> findBySequence_Name(@PathVariable String theId) {
+	public List<GA4GH> findBySequence_Name(@RequestHeader("key") String language,
+			@PathVariable String theId) {
+		System.out.println(language);
+		System.out.println(language);
+		System.out.println(language);
+		System.out.println(language);
+		System.out.println(language);
 		
-		return GA4GHService.findBySequence_Name(theId);
+		if(language.equals("ga4ahapisecuritykey"))
+			return GA4GHService.findBySequence_Name(theId);
+		
+		return null;
+		
+
 	}
 
 	@GetMapping("/genbank/{theId}")
-	public List<GA4GH> findByGenBank_Accn(@PathVariable String theId) {
+	public List<GA4GH> findByGenBank_Accn(@RequestHeader("key") String language,
+			@PathVariable String theId) {
 		
-		return GA4GHService.findByGenBank_Accn(theId);
+		System.out.println(language);
+		System.out.println(language);
+		System.out.println(language);
+		System.out.println(language);
+		System.out.println(language);
+		
+		if(language.equals("ga4ahapisecuritykey"))
+			return GA4GHService.findBySequence_Name(theId);
+		
+		return null;
 	}
 
 	@GetMapping("/refseq/{theId}")
