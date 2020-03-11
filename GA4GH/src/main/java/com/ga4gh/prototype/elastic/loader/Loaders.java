8165c6c20973cee9ad8e0faefc9b979f7ea01e76
soumyadip007 <soumyadip.note@gmail.com>
2020-03-11 20:23:01 +0530

    @PostContruct

--- GA4GH/src/main/java/com/ga4gh/prototype/elastic/loader/Loaders.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/elastic/loader/Loaders.java
@@ -1,9 +1,12 @@
 package com.ga4gh.prototype.elastic.loader;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.stereotype.Component;
 
+import com.ga4gh.prototype.entity.GA4GH;
 import com.ga4gh.prototype.service.GA4GHService;
 
 @Component
@@ -16,4 +19,10 @@ public class Loaders {
 	@Autowired  
 	private GA4GHService GA4GHService;
 	
+	
+	@PostConstruct
+	public void loadAll()
+	{
+		operations.putMapping(GA4GH.class);
+	}
 }
