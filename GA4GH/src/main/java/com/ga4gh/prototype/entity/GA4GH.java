c8ca6dbff7d68f5e5808499114d94e106df834d1
soumyadip007 <soumyadip.note@gmail.com>
2020-03-11 20:28:20 +0530

    Putmapping Failed

--- GA4GH/src/main/java/com/ga4gh/prototype/entity/GA4GH.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/entity/GA4GH.java
@@ -18,7 +18,7 @@ import lombok.Setter;
 @Setter	
 @Entity
 @Table(name = "ga4gh")
-@Document(indexName = "ga4gh", type="ga4gh", shards=1)
+@Document(indexName = "GA4GH", type="GA4GH", shards=1)
 public class GA4GH {
 
 
