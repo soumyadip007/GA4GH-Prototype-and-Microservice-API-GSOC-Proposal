d6d944bdf86c5d667340537d64a5b82c80190b42
soumyadip007 <soumyadip.note@gmail.com>
2020-03-11 20:17:26 +0530

    Autowired added

--- GA4GH/src/main/java/com/ga4gh/prototype/elastic/loader/Loaders.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/elastic/loader/Loaders.java
@@ -1,8 +1,13 @@
 package com.ga4gh.prototype.elastic.loader;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Loaders {
 
+	@Autowired
+	ElasticsearchOperations operations;
+	
 }
