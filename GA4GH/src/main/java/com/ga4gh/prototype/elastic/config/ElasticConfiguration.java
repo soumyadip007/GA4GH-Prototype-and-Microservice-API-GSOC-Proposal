24ea75ddea2ec259ff89188b73faf795ce8675f5
soumyadip007 <soumyadip.note@gmail.com>
2020-03-11 21:31:46 +0530

    Elastic search config

--- GA4GH/src/main/java/com/ga4gh/prototype/elastic/config/ElasticConfiguration.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/elastic/config/ElasticConfiguration.java
@@ -3,45 +3,94 @@ package com.ga4gh.prototype.elastic.config;
 import java.io.File;
 import java.io.IOException;
 
+import com.google.gson.Gson;
+import com.kodcu.prop.ConfigProps;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.node.NodeBuilder;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.context.annotation.Profile;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.TimeUnit;
 @Configuration
 @EnableJpaRepositories(basePackages ="com.ga4gh.prototype.repository")
 @EnableElasticsearchRepositories(basePackages ="com.ga4gh.prototype.elastic.repository")
 public class ElasticConfiguration {
 
+	  @Autowired
+	    private ConfigProps props;
 
-    @Bean
-    public NodeBuilder nodeBuilder() {
-        return new NodeBuilder();
-    }
+	    @Profile("production")
+	    @Bean(destroyMethod = "close")
+	    @SuppressWarnings("all")
+	    public TransportClient getTransportClient() throws UnknownHostException {
+	        return new PreBuiltTransportClient(Settings.EMPTY)
+	                    .addTransportAddress(new TransportAddress(InetAddress.getByName(props.getClients().getHostname()),
+	                            props.getClients().getTransportPort()));
+	    }
 
-    @Bean
-    public ElasticsearchOperations elasticsearchTemplate() throws IOException {
-        File tmpDir = File.createTempFile("elastic", Long.toString(System.nanoTime()));
-        System.out.println("Temp directory: " + tmpDir.getAbsolutePath());
-        Settings.Builder elasticsearchSettings =
-                Settings.settingsBuilder()
-                        .put("http.enabled", "true") // 1
-                        .put("index.number_of_shards", "1")
-                        .put("path.data", new File(tmpDir, "data").getAbsolutePath()) // 2
-                        .put("path.logs", new File(tmpDir, "logs").getAbsolutePath()) // 2
-                        .put("path.work", new File(tmpDir, "work").getAbsolutePath()) // 2
-                        .put("path.home", tmpDir); // 3
+	    @Profile({"production", "docker"})
+	    @Bean(destroyMethod = "close")
+	    public RestHighLevelClient getRestClient() {
+	        return new RestHighLevelClient(RestClient.builder(new HttpHost(props.getClients().getHostname(),
+	                props.getClients().getHttpPort(), props.getClients().getScheme())));
+	    }
 
+	    @Profile("test")
+	    @Bean(destroyMethod = "close")
+	    public RestHighLevelClient getRestClientForTest() {
+	        return new RestHighLevelClient(RestClient.builder(new HttpHost(getFixedHostPortGenericContainer().getContainerIpAddress(),
+	                props.getClients().getHttpPort())).setHttpClientConfigCallback(httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(getCredentialsProvider())));
+	    }
 
+	    @Bean
+	    public SearchSourceBuilder getSearchSourceBuilder(){
+	        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+	        sourceBuilder.from(props.getIndex().getFrom());
+	        sourceBuilder.size(props.getIndex().getSize());
+	        sourceBuilder.timeout(new TimeValue(props.getIndex().getTimeout(), TimeUnit.SECONDS));
 
-        return new ElasticsearchTemplate(nodeBuilder()
-                .local(true)
-                .settings(elasticsearchSettings.build())
-                .node()
-                .client());
-    }
+	        return sourceBuilder;
+	    }
+
+	    @Bean
+	    public Gson getGson(){
+	        return new Gson();
+	    }
+
+	    @Profile("test")
+	    @Bean(destroyMethod = "stop")
+	    public FixedHostPortGenericContainer getFixedHostPortGenericContainer(){
+
+	        String url = String.join(":", props.getElastic().getImageUrl(), props.getElastic().getVersion());
+
+	        FixedHostPortGenericContainer fixed = new FixedHostPortGenericContainer(url);
+	        fixed.withFixedExposedPort(props.getClients().getHttpPort(), props.getClients().getContainerPort());
+	        fixed.waitingFor(Wait.forHttp("/"));
+	        fixed.start();
+
+	        return fixed;
+	    }
+
+	    @Bean
+	    public BasicCredentialsProvider getCredentialsProvider(){
+	        final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+	        credentialsProvider.setCredentials(AuthScope.ANY,
+	                new UsernamePasswordCredentials(props.getElastic().getCredentialUsername(), props.getElastic().getCredentialPassword()));
+	        return credentialsProvider;
+	    }
 }
