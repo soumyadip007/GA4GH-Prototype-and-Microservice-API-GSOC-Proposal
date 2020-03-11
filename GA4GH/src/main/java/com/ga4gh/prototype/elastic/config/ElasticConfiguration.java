a11bbe08d6c79048b9e34f21831b1d6182445b24
soumyadip007 <soumyadip.note@gmail.com>
2020-03-11 20:13:50 +0530

    Elastic Config

--- GA4GH/src/main/java/com/ga4gh/prototype/elastic/config/ElasticConfiguration.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/elastic/config/ElasticConfiguration.java
@@ -1,10 +1,12 @@
-package com.ga4gh.prototype.config;
+package com.ga4gh.prototype.elastic.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
+import org.elasticsearch.node.*;;
 @Configuration
 @EnableElasticsearchRepositories(basePackages ="com.ga4gh.prototype.dao")
 public class ElasticConfiguration {
+
 
 }
