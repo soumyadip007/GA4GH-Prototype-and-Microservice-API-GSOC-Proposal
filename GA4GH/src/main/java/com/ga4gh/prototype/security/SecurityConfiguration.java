b5e529a289097c1fc92be16d183fa4a3c85e2010
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 22:41:28 +0530

    AuthenticationManage instantiated

--- GA4GH/src/main/java/com/ga4gh/prototype/security/SecurityConfiguration.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/security/SecurityConfiguration.java
@@ -1,10 +1,8 @@
 package com.ga4gh.prototype.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -44,4 +42,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	{
 		return NoOpPasswordEncoder.getInstance();
 	}
+	
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception{
+		return super.authenticationManagerBean();
+	}
+	
 }
