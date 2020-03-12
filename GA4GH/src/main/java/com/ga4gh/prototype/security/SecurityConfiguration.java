f3670015c0d83592b8aa235a753db76bc1518236
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 22:42:41 +0530

    SessionCreationPolicy Disabled

--- GA4GH/src/main/java/com/ga4gh/prototype/security/SecurityConfiguration.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -32,7 +33,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 		http.csrf().disable()
 			.authorizeRequests().antMatchers("/authenticate").permitAll()
-			.anyRequest().authenticated();
+			.anyRequest().authenticated()
+			.and().sessionManagement().
+			sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
 	
