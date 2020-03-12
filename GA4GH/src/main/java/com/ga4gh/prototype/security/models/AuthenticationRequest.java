7bcd9526a2f290f6d55da0ef6be57f0edd530452
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 21:59:52 +0530

    Lombok properties added

--- GA4GH/src/main/java/com/ga4gh/prototype/security/models/AuthenticationRequest.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/security/models/AuthenticationRequest.java
@@ -1,5 +1,12 @@
 package com.ga4gh.prototype.security.models;
 
+import lombok.Getter;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationRequest {
 
 	private String username;
