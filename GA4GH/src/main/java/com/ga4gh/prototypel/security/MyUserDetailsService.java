650ab414b3949904cfd8bcf526f232c32e2873b4
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 21:02:53 +0530

    loadUserByUsername() added into security

--- GA4GH/src/main/java/com/ga4gh/prototypel/security/MyUserDetailsService.java
+++ GA4GH/src/main/java/com/ga4gh/prototypel/security/MyUserDetailsService.java
@@ -1,5 +1,17 @@
 package com.ga4gh.prototypel.security;
 
-public class MyUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+public class MyUserDetailsService implements UserDetailsService  {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws
+											UsernameNotFoundException{
+											
+		return null;
+		
+	}
+	
 }
