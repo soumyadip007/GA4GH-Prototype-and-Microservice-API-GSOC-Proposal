027724c0a7e00c7a5513ed1f474bdf48a0d7e688
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 22:04:16 +0530

    start() addded

--- GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/JWTRestController.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/JWTRestController.java
@@ -1,7 +1,16 @@
 package com.ga4gh.prototype.restcontroller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JWTRestController {
 
+	@RequestMapping("/start")
+	public String start()
+	{
+		return "GA4GH";
+	}
+	
+	
+	
 }
