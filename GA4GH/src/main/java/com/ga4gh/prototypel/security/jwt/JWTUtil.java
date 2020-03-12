00ad6e952ed0699817e053bccacfd4b6c0b4b371
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 21:48:07 +0530

    createToken(Map<String, Object> claims, String subject) JWT

--- GA4GH/src/main/java/com/ga4gh/prototypel/security/jwt/JWTUtil.java
+++ GA4GH/src/main/java/com/ga4gh/prototypel/security/jwt/JWTUtil.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service 
 public class JWTUtil {
@@ -45,7 +46,9 @@ public class JWTUtil {
 	
 	private String createToken(Map<String, Object> claims, String subject) {
 		return Jwts.builder().setClaims(claims).setSubject(subject)
-				.setIssuedAt(new Date(System.currentTimeMillis())).
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis()+1000*60*60*10))
+				.signWith(SignatureAlgorithm.HS256,Secret_Key).compact();
 				
 	}
 }
