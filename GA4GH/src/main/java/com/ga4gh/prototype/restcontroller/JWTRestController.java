6e37c6bf314f45171a7e1ac664ad065034c817f3
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 22:10:22 +0530

    AuthenticationRequest() getter setter added, lombok rm

--- GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/JWTRestController.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/JWTRestController.java
@@ -4,6 +4,7 @@ import org.elasticsearch.client.security.AuthenticateRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,10 +23,9 @@ public class JWTRestController {
 	}
 	
 	@RequestMapping(value="/authenticate", method=RequestMethod.POST)
-	public ResponseEntity
-		(@RequestBody AuthenticateRequest authenticateRequest) throws Exception
+	public ResponseEntity(@RequestBody AuthenticateRequest authenticateRequest) throws Exception
 	{
-		return "GA4GH";
+		return UsernamePasswordAuthenticationToken(authenticateRequest.get
 	}
 	
 	
--- GA4GH/src/main/java/com/ga4gh/prototype/security/models/AuthenticationRequest.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/security/models/AuthenticationRequest.java
@@ -3,13 +3,32 @@ package com.ga4gh.prototype.security.models;
 import lombok.Getter;
 import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class AuthenticationRequest {
 
 	private String username;
 	private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public AuthenticationRequest(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	public AuthenticationRequest() {
+		super();
+	}
+	
+	
 	
 }
