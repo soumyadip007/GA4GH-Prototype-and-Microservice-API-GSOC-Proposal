5aa693defeac6f35c7b08a66fb46a407f4d76032
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 21:09:12 +0530

    MyUserDetailsService added to @Service

--- GA4GH/src/main/java/com/ga4gh/prototypel/security/MyUserDetailsService.java
+++ GA4GH/src/main/java/com/ga4gh/prototypel/security/MyUserDetailsService.java
@@ -6,14 +6,17 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class MyUserDetailsService implements UserDetailsService  {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws
 											UsernameNotFoundException{
 											
-		return new User("Soumya","Soumya",new ArrayList<>());
+		return new User("soumya","soumya",new ArrayList<>());
 		
 	}
 	
