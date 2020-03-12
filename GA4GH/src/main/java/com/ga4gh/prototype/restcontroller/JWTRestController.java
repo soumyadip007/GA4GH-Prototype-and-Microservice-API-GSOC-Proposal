f42c8d7b7aa5ef81c8c9bc854be1548881b7ec74
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 22:08:06 +0530

    AuthenticationManager Autowired

--- GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/JWTRestController.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/JWTRestController.java
@@ -1,6 +1,9 @@
 package com.ga4gh.prototype.restcontroller;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.elasticsearch.client.security.AuthenticateRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JWTRestController {
 
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
 	@RequestMapping("/start")
 	public String start()
 	{
