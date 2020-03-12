26b0684202d4a266c4baf3c7819ca5d30a714ae3
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 23:34:57 +0530

    @Configuration Annotation Added

--- GA4GH/src/main/java/com/ga4gh/prototype/security/SecurityConfiguration.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.ga4gh.prototype.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * @github soumyadip007
  *
  */
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
