a6521c85756140453fb81c62bfae6c02c1e9eb66
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 21:18:58 +0530

    (exclude = {SecurityAutoConfiguration.class }) remoced

--- GA4GH/src/main/java/com/ga4gh/prototype/Ga4GhApplication.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/Ga4GhApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
  * @github soumyadip007
  *
  */
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class Ga4GhApplication {
 
 	public static void main(String[] args) {
