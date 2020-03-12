926f57202851983cb89b13c16100f2b98e464b70
soumyadip007 <soumyadip.note@gmail.com>
2020-03-13 00:00:43 +0530

    filterChain added

--- GA4GH/src/main/java/com/ga4gh/prototype/security/jwt/filter/JWTRequestFilter.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/security/jwt/filter/JWTRequestFilter.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -59,6 +58,7 @@ public class JWTRequestFilter extends OncePerRequestFilter {
 				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
 			}
 		}
+		filterChain.doFilter(request,response);
 	}
 
 }
