acaa7aca78624653d4765692d9a919b54c701da3
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 23:50:04 +0530

    CopyR

--- GA4GH/src/main/java/com/ga4gh/prototype/security/jwt/filter/JWTRequestFilter.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/security/jwt/filter/JWTRequestFilter.java
@@ -9,12 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.filter.OncePerRequestFilter;
 
+/**
+ * 
+ * @author Soumyadip Chowdhury
+ * @github soumyadip007
+ *
+ */
 public class JWTRequestFilter extends OncePerRequestFilter {
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		
 		
 	}
 
