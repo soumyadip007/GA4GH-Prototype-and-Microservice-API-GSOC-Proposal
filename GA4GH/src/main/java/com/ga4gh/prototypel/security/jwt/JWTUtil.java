7e17303195de83fded8828c30c8f7808e5216fa3
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 21:30:45 +0530

    extractExpiration()

--- GA4GH/src/main/java/com/ga4gh/prototypel/security/jwt/JWTUtil.java
+++ GA4GH/src/main/java/com/ga4gh/prototypel/security/jwt/JWTUtil.java
@@ -1,11 +1,23 @@
 package com.ga4gh.prototypel.security.jwt;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
 
 @Service 
 public class JWTUtil {
 
 	private String Secret_Key="secret";
+	
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+	
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExperiation);
+	}
 	
 	
 }
