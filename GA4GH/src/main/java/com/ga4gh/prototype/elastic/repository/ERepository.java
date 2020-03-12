48f8174aec9051b24e5d972814a9a302fde130a3
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 10:23:13 +0530

    ERepository added

--- GA4GH/src/main/java/com/ga4gh/prototype/elastic/repository/ERepository.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/elastic/repository/ERepository.java
@@ -1,11 +1,11 @@
-//package com.ga4gh.prototype.elastic.repository;
-//
-//import java.util.List;
-//
-//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import com.ga4gh.prototype.entity.GA4GH;
-//@Repository
-//public interface ERepository extends ElasticsearchRepository<GA4GH, Integer> {
-//}
+package com.ga4gh.prototype.elastic.repository;
+
+import java.util.List;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ga4gh.prototype.entity.GA4GH;
+@Repository("ERepository")
+public interface ERepository extends ElasticsearchRepository<GA4GH, Integer> {
+}
