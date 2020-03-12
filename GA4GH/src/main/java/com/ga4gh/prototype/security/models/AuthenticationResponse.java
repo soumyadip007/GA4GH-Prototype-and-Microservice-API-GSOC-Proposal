eed5ad905fdcb5b6838f3075c5639cde884514c6
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 22:01:39 +0530

    Enitity and Getter setters added

--- GA4GH/src/main/java/com/ga4gh/prototype/security/models/AuthenticationResponse.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/security/models/AuthenticationResponse.java
@@ -1,5 +1,15 @@
 package com.ga4gh.prototype.security.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationResponse {
 
+	private String jwt;
 }
