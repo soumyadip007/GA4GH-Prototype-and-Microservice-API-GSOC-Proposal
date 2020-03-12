92d60f8aaf77b9a759d52ecd39855f82cdcf7172
soumyadip007 <soumyadip.note@gmail.com>
2020-03-12 21:10:43 +0530

    passwordEncoder() added

--- GA4GH/src/main/java/com/ga4gh/prototypel/security/SecurityConfiguration.java
+++ GA4GH/src/main/java/com/ga4gh/prototypel/security/SecurityConfiguration.java
@@ -6,13 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 
 /**
  * 
@@ -36,8 +33,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	}
 	
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
