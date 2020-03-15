c893ac383a13149a5115ad4895430306b8a96043
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 13:28:35 +0530

    Mono ga4ghMono

--- GA4GH/src/test/java/com/ga4gh/prototype/testing/FluxAndMono/MonoTest.java
+++ GA4GH/src/test/java/com/ga4gh/prototype/testing/FluxAndMono/MonoTest.java
@@ -2,11 +2,13 @@ package com.ga4gh.prototype.testing.FluxAndMono;
 
 import org.junit.Test;
 
+import reactor.core.publisher.Mono;
+
 public class MonoTest {
 
 	@Test
 	public void monoTest()
 	{
-		
+		Mono ga4ghMono=Mono.just("GA4GH");
 	}
 }
