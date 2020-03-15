6e5b4ca3307b9f7dafc97c765d958ef7dd5f4bb5
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 12:29:33 +0530

    @Test Method added

--- GA4GH/src/test/java/com/ga4gh/prototype/testing/FluxAndMono/FluxAndMonoTest.java
+++ GA4GH/src/test/java/com/ga4gh/prototype/testing/FluxAndMono/FluxAndMonoTest.java
@@ -1,5 +1,16 @@
 package com.ga4gh.prototype.testing.FluxAndMono;
 
+import org.junit.Test;
+
+import reactor.core.publisher.Flux;
+
 public class FluxAndMonoTest {
 
+	
+	@Test
+	public void fluxTest()
+	{
+		Flux.just("Spring Boot","Spring MVC", "GA4GH");
+		
+	}
 }
