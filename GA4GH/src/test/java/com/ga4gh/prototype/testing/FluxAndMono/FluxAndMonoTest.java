4a3ef3291b73dc4dd76b9ee543a30b07d5578887
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 12:39:32 +0530

    @UnitTest Flux

--- GA4GH/src/test/java/com/ga4gh/prototype/testing/FluxAndMono/FluxAndMonoTest.java
+++ GA4GH/src/test/java/com/ga4gh/prototype/testing/FluxAndMono/FluxAndMonoTest.java
@@ -10,8 +10,12 @@ public class FluxAndMonoTest {
 	@Test
 	public void fluxTest()
 	{
-		Flux<String> ga4ghFlux=Flux.just("GA4GH","genbankaccn","refseqaccn","sequencelength",
-				"sequencename").concatWith(Flux.error(new RuntimeException("Exception Occ")));
+		Flux<String> ga4ghFlux=Flux.just("GA4GH",
+				"genbankaccn","refseqaccn","sequencelength","sequencename")
+				.concatWith(Flux.error(new RuntimeException("Exception Occ")))
+				.log();
+		
+		
 		ga4ghFlux
 			.subscribe(System.out::println,
 					(e)->System.err.println(e)
