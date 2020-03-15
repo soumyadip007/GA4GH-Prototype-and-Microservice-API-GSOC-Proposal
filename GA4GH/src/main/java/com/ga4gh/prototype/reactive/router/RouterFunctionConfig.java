0cd1803b0d92502046e93c22e9aed4c06fd1504a
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 18:16:26 +0530

    RouteFunction COnfig And Route

--- GA4GH/src/main/java/com/ga4gh/prototype/reactive/router/RouterFunctionConfig.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/reactive/router/RouterFunctionConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import com.ga4gh.prototype.reactive.handler.GA4GHReactiveHandler;
+
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
@@ -19,12 +20,13 @@ public class RouterFunctionConfig {
 	public RouterFunction<ServerResponse> route(GA4GHReactiveHandler reactiveHandler)
 	{
 		return RouterFunctions
-			.route(GET("/f/f")
+			.route(GET("/ga4gh/flux")
 					.and(accept(MediaType.APPLICATION_JSON))
-							,reactiveHandler::flux);			
+							,reactiveHandler::flux)
+			.andRoute(GET("/ga4gh/mono")
+					.and(accept(MediaType.APPLICATION_JSON))
+							,reactiveHandler::mono);
+	
 	}
 
-
-	
-	
 }
