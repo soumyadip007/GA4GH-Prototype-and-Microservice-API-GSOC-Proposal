a90bcc1e41151a1f7d0f4636ec70671f96e6386d
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 14:47:03 +0530

    Flux Controller Test

--- GA4GH/src/test/java/com/ga4gh/prototype/testing/FluxAndMono/FluxControllerTest.java
+++ GA4GH/src/test/java/com/ga4gh/prototype/testing/FluxAndMono/FluxControllerTest.java
@@ -1,16 +1,41 @@
 package com.ga4gh.prototype.testing.FluxAndMono;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
 @RunWith(SpringRunner.class)
-@WebFluxTest
-public class FluxController {
+@WebFluxTest(controllers = FluxControllerTest.class)
+public class FluxControllerTest {
 
 	@Autowired
 	WebTestClient webTestClient;
 	
+	
+	@Test
+	public void flux_approch1() {
+		
+		Flux<String> responseBody = webTestClient.get().uri("/flux/stream")
+		.accept(MediaType.APPLICATION_JSON_UTF8)
+		.exchange()
+		.expectStatus().isOk()
+		.returnResult(String.class)
+		.getResponseBody();
+		
+		StepVerifier.create(responseBody)
+		.expectSubscription()
+		.expectNext("GA4GH")
+		.expectNext("genbankaccn")
+		.expectNext("refseqaccn")
+		.expectNext("sequencelength")
+		.verifyComplete();  
+		
+	}
 }
