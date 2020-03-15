30701ee773383e89cdfbfaa8f57af727ab90c521
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 14:23:47 +0530

    Delay Added

--- GA4GH/src/main/java/com/ga4gh/prototype/reactivecontroller/FluxController.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/reactivecontroller/FluxController.java
@@ -1,5 +1,7 @@
 package com.ga4gh.prototype.reactivecontroller;
 
+import java.time.Duration;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,8 @@ public class FluxController {
 	public Flux<String> ga4ghFlux()
 	{
 		return Flux.just("GA4GH",
-				"genbankaccn","refseqaccn","sequencelength")
+				" genbankaccn"," refseqaccn"," sequencelength")
+				.delayElements(Duration.ofSeconds(1))
 				.log();
 		
 	}
