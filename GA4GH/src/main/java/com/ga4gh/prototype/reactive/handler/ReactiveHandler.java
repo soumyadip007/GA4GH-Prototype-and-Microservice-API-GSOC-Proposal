5abebe0b5db8a92d9d391171fd6065c818b96620
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 17:23:11 +0530

    ServerResponse & serverRequest

--- GA4GH/src/main/java/com/ga4gh/prototype/reactive/handler/ReactiveHandler.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/reactive/handler/ReactiveHandler.java
@@ -1,8 +1,16 @@
 package com.ga4gh.prototype.reactive.handler;
 
+import org.omg.CORBA.ServerRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import reactor.core.publisher.Mono;
 
 @Component
 public class ReactiveHandler {
 
+	public Mono<ServerResponse> flux(ServerRequest serverRequest){
+		
+	}
+	
 }
