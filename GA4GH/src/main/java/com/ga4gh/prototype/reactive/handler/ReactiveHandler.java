14560b9225487205452f73031fc164a74b66a146
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 17:24:59 +0530

    ServerResponse Added

--- GA4GH/src/main/java/com/ga4gh/prototype/reactive/handler/ReactiveHandler.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/reactive/handler/ReactiveHandler.java
@@ -1,9 +1,11 @@
 package com.ga4gh.prototype.reactive.handler;
 
 import org.omg.CORBA.ServerRequest;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -11,6 +13,12 @@ public class ReactiveHandler {
 
 	public Mono<ServerResponse> flux(ServerRequest serverRequest){
 		
+		return ServerResponse.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(
+				Flux.just("GA4GH"," genbankaccn"," refseqaccn",
+					" sequencelength").log(), String.class);
+				
 	}
 	
 }
