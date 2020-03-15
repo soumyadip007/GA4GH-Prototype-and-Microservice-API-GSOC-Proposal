f76149a9b4d93a34062fe692a4d0017e5ac365e1
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 14:26:04 +0530

    JSON Media Type added

--- GA4GH/src/main/java/com/ga4gh/prototype/reactivecontroller/FluxController.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/reactivecontroller/FluxController.java
@@ -2,8 +2,8 @@ package com.ga4gh.prototype.reactivecontroller;
 
 import java.time.Duration;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
@@ -13,6 +13,16 @@ public class FluxController {
 
 	@GetMapping("/flux")
 	public Flux<String> ga4ghFlux()
+	{
+		return Flux.just("GA4GH",
+				" genbankaccn"," refseqaccn"," sequencelength")
+				.delayElements(Duration.ofSeconds(1))
+				.log();
+		
+	}
+	
+	@GetMapping(value="/fluxstream", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Flux<String> ga4ghFluxStream()
 	{
 		return Flux.just("GA4GH",
 				" genbankaccn"," refseqaccn"," sequencelength")
