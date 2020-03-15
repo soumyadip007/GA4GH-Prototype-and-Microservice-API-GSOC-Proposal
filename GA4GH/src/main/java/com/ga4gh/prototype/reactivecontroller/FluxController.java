266b4a5b547a096417bba7574bb06cdff22703f9
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 14:34:21 +0530

    Endpoints Changed

--- GA4GH/src/main/java/com/ga4gh/prototype/reactivecontroller/FluxController.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/reactivecontroller/FluxController.java
@@ -1,17 +1,27 @@
 package com.ga4gh.prototype.reactivecontroller;
 
 import java.time.Duration;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ga4gh.prototype.entity.GA4GH;
+import com.ga4gh.prototype.service.GA4GHService;
 
 import reactor.core.publisher.Flux;
 
 @RestController
+@RequestMapping("/flux")
 public class FluxController {
 
-	@GetMapping("/flux")
+	@Autowired  
+	private GA4GHService GA4GHService;
+
+	@GetMapping("")
 	public Flux<String> ga4ghFlux()
 	{
 		return Flux.just("GA4GH",
@@ -21,11 +31,22 @@ public class FluxController {
 		
 	}
 	
-	@GetMapping(value="/fluxstream", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value="/stream", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Flux<String> ga4ghFluxStream()
 	{
 		return Flux.just("GA4GH",
 				" genbankaccn"," refseqaccn"," sequencelength")
+				.delayElements(Duration.ofSeconds(1))
+				.log();
+		
+	}
+	
+	
+	@GetMapping(value="/xGA4GH", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Flux<List<GA4GH>> ga4ghFluxaLL()
+	{
+		List<GA4GH> ls=GA4GHService.findAll();
+		return Flux.just(ls)
 				.delayElements(Duration.ofSeconds(1))
 				.log();
 		
