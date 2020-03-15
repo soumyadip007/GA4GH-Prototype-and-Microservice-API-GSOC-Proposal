0d6e4a255ca825ce527eb484a09b32180c5c7d1d
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 12:37:55 +0530

    Addmin Error As and Event

--- GA4GH/src/test/java/com/ga4gh/prototype/testing/FluxAndMono/FluxAndMonoTest.java
+++ GA4GH/src/test/java/com/ga4gh/prototype/testing/FluxAndMono/FluxAndMonoTest.java
@@ -10,9 +10,11 @@ public class FluxAndMonoTest {
 	@Test
 	public void fluxTest()
 	{
-		Flux<String> ga4ghFlux=Flux.just("GA4GH","genbankaccn","refseqaccn","sequencelength","sequencename");
-		
+		Flux<String> ga4ghFlux=Flux.just("GA4GH","genbankaccn","refseqaccn","sequencelength",
+				"sequencename").concatWith(Flux.error(new RuntimeException("Exception Occ")));
 		ga4ghFlux
-			.subscribe(System.out::println);
+			.subscribe(System.out::println,
+					(e)->System.err.println(e)
+					);
 	}
 }
