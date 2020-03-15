af517dddc9b276d2085803ed5ca48bd2ddcfdbbc
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 17:39:38 +0530

    Handler Mapping

--- GA4GH/src/main/java/com/ga4gh/prototype/reactive/router/RouterFunctionConfig.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/reactive/router/RouterFunctionConfig.java
@@ -2,17 +2,29 @@ package com.ga4gh.prototype.reactive.router;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import com.ga4gh.prototype.reactive.handler.ReactiveHandler;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
 
 @Configuration
 public class RouterFunctionConfig {
 	
 	@Bean
-	public RouterFunction<ServerResponse> route()
+	public RouterFunction<ServerResponse> route(ReactiveHandler reactiveHandler)
 	{
-		
+		return RouterFunctions
+			.route(GET("/f/f")
+					.and(accept(MediaType.APPLICATION_JSON))
+							,reactiveHandler::flux);			
 	}
+
+
 	
 	
 }
