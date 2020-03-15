45c1d1a3aebe0e4e3bae2e6deabf77c0833a7ebd
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 14:19:25 +0530

    Flux Controller Config

--- GA4GH/src/main/java/com/ga4gh/prototype/reactivecontroller/FluxController.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/reactivecontroller/FluxController.java
@@ -1,14 +1,16 @@
-package com.ga4gh.prototypr.reactivecontroller;
+package com.ga4gh.prototype.reactivecontroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
 
 @RestController
+@RequestMapping("/flux")
 public class FluxController {
 
-	@GetMapping("/flux")
+	@GetMapping("/hello")
 	public Flux<String> ga4ghFlux()
 	{
 		return Flux.just("GA4GH",
