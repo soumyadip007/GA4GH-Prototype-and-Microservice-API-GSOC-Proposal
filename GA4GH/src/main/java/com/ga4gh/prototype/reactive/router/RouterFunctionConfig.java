9129b9f51c882882aff19891db1dda01a8061116
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 17:26:34 +0530

    RouterFunctionConfig

---
+++ GA4GH/src/main/java/com/ga4gh/prototype/reactive/router/RouterFunctionConfig.java
@@ -0,0 +1,18 @@
+package com.ga4gh.prototype.reactive.router;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class RouterFunctionConfig {
+	
+	@Bean
+	public RouterFunction<ServerResponse> route()
+	{
+		
+	}
+	
+	
+}
