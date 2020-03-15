4e538058eca91a96f46f1233c193a83263550947
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 14:37:33 +0530

    WebTestClient @Autowired

--- GA4GH/src/test/java/com/ga4gh/prototype/testing/FluxAndMono/FluxController.java
+++ GA4GH/src/test/java/com/ga4gh/prototype/testing/FluxAndMono/FluxController.java
@@ -1,11 +1,16 @@
 package com.ga4gh.prototype.testing.FluxAndMono;
 
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @WebFluxTest
 public class FluxController {
 
+	@Autowired
+	WebTestClient webTestClient;
+	
 }
