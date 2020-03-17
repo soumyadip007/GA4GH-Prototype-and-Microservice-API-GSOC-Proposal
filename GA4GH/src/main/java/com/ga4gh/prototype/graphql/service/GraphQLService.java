48d6ed3f799bf37478e05d66bee1231139a9fe2f
soumyadip007 <soumyadip.note@gmail.com>
2020-03-18 00:56:54 +0530

    RuntimeWiring added

--- GA4GH/src/main/java/com/ga4gh/prototype/graphql/service/GraphQLService.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/graphql/service/GraphQLService.java
@@ -1,9 +1,15 @@
 package com.ga4gh.prototype.graphql.service;
 
-import javax.annotation.Resource;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.dom4j.datatype.SchemaParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import com.ga4gh.prototype.graphql.datafetcher.AllGA4GHDataFetcher;
@@ -11,6 +17,11 @@ import com.ga4gh.prototype.graphql.datafetcher.GA4GHDataFetcher;
 import com.ga4gh.prototype.repository.GA4GHRepository;
 
 import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
 
 @Service
 public class GraphQLService {
@@ -29,6 +40,29 @@ public class GraphQLService {
 	
 	private GraphQL graphQL;
 	
+	@PostConstruct
+	private void loadSchema() throws IOException{
+		
+		
+		File schemaFile=resource.getFile();
+		  
+		TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(schemaFile);
+	    RuntimeWiring wiring = buildRuntimeWiring();
+	    GraphQLSchema schema = new SchemaGenerator().makeExecutableSchema(typeRegistry, wiring);
+	    graphQL = GraphQL.newGraphQL(schema).build();
+		
+	}
+
+	 private RuntimeWiring buildRuntimeWiring() {
+	        return RuntimeWiring.newRuntimeWiring()
+	                .type("Query", typeWiring -> typeWiring
+	                        .dataFetcher("allData", allGA4GHDataFetcher)
+	                        .dataFetcher("ga4gh", ga4ghDataFetcher))
+	                .build();
+	    }
 	
+    public GraphQL getGraphQL() {
+        return graphQL;
+    }
 	
 }
