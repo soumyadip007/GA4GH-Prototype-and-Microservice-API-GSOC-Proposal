7586cf9339393d75dae8cdbdfc262dc5729b19b5
soumyadip007 <soumyadip.note@gmail.com>
2020-03-18 00:32:57 +0530

    @Autowired GA4GHRepositor

--- GA4GH/src/main/java/com/ga4gh/prototype/graphql/service/GraphQLService.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/graphql/service/GraphQLService.java
@@ -1,8 +1,13 @@
 package com.ga4gh.prototype.graphql.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ga4gh.prototype.repository.GA4GHRepository;
 
 @Service
 public class GraphQLService {
 
+	@Autowired
+	GA4GHRepository ga4ghRepository;
 }
