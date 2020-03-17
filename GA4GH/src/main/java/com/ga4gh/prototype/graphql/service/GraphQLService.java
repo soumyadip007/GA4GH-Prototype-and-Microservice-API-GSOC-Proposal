9e2aba4271d6579d77ef48303c020d5f696cd979
soumyadip007 <soumyadip.note@gmail.com>
2020-03-18 00:34:05 +0530

    Resource Value added

--- GA4GH/src/main/java/com/ga4gh/prototype/graphql/service/GraphQLService.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/graphql/service/GraphQLService.java
@@ -1,6 +1,9 @@
 package com.ga4gh.prototype.graphql.service;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.ga4gh.prototype.repository.GA4GHRepository;
@@ -10,4 +13,8 @@ public class GraphQLService {
 
 	@Autowired
 	GA4GHRepository ga4ghRepository;
+	
+	@Value("classpath:ga4gh.graphql")
+	Resource resource;
+	
 }
