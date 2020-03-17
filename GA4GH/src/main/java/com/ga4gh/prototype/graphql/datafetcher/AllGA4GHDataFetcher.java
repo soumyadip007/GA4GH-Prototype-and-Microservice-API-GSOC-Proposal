a854041b47d002cd00e6099af116cb04c61d29c7
soumyadip007 <soumyadip.note@gmail.com>
2020-03-18 00:41:36 +0530

    Datafetcher implemented

--- GA4GH/src/main/java/com/ga4gh/prototype/graphql/datafetcher/AllGA4GHDataFetcher.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/graphql/datafetcher/AllGA4GHDataFetcher.java
@@ -1,5 +1,18 @@
 package com.ga4gh.prototype.graphql.datafetcher;
 
-public class AllGA4GHDataFetcher {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ga4gh.prototype.entity.GA4GH;
+import com.ga4gh.prototype.repository.GA4GHRepository;
+
+import graphql.schema.DataFetcher;
+
+@Component
+public class AllGA4GHDataFetcher implements DataFetcher<List<GA4GH>> {
+
+	@Autowired
+	GA4GHRepository ga4ghRepository;
 }
