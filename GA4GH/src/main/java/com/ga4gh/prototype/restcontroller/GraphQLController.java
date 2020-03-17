d1d1ef19ec8860db5d7358b1b7e352e0fda01527
soumyadip007 <soumyadip.note@gmail.com>
2020-03-18 01:04:54 +0530

    Graph Controller Added

--- GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/GraphQLController.java
+++ GA4GH/src/main/java/com/ga4gh/prototype/restcontroller/GraphQLController.java
@@ -1,12 +1,30 @@
 package com.ga4gh.prototype.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ga4gh.prototype.graphql.service.GraphQLService;
+
+import graphql.ExecutionResult;
 
 @CrossOrigin(origins ="*",allowedHeaders="*",maxAge=200000) 
 @RestController
 @RequestMapping("/graph")
 public class GraphQLController {
 
+	@Autowired
+    GraphQLService graphQLService;
+	
+	 @PostMapping
+	 public ResponseEntity<Object> graph(@RequestBody String query) {
+	        ExecutionResult execute = graphQLService.getGraphQL().execute(query);
+
+	        return new ResponseEntity<>(execute, HttpStatus.OK);
+	    }
 }
